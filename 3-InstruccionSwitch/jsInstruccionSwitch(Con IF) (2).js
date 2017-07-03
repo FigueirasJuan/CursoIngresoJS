231bf62b3d546e50d81efc753f22928655312a85
FigueirasJuan <tec.jmfigueiras@gmail.com>
2017-07-03 12:17:01 -0300

    Ejercicios de Switchs terminados

---
+++ 3-InstruccionSwitch/jsInstruccionSwitch(Con IF) (2).js
@@ -0,0 +1,19 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById("mes").value;
+//alert (mesDelAño);
+if(mesDelAño == "Julio" || mesDelAño == "Agosto")
+{
+	alert("Abrigate que hace frío.");
+}
+else if(mes == "Septiembre" || mes == "Octubre" || mes == "Noviembre" || mes == "Diciembre")
+{
+    alert("Ya pasamos el frío, ahora calor!!!.");
+}    
+else if(mes == "Enero" || mes == "Febrero" || mes == "Marzo" || mes == "Abril" || mes == "Mayo" || mes == "Junio" )
+{
+  alert("Falta para el invierno.");
+}	
+
+}//FIN DE LA FUNCIÓN
