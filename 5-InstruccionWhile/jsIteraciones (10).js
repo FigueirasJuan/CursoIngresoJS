2d23e04f807e9c7edc3261aa19078160cf73e92e
FigueirasJuan <tec.jmfigueiras@gmail.com>
2017-07-10 12:18:31 -0300

    Ejercicio 10 incompleto.

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,23 +1,51 @@
 function Mostrar()
 {
 
-	var contador=0;
-	var sumanega=0;
-	var cantnega=0;
-	var sumaposi=0;
-	var cantposi=0;
-	var cantceros=0;
-	var cantpares=0;
-	var promposi=0;
-	var promnega=0;
-	var difposinega;
-	var respuesta= true;
+	var respuesta = true;
+	var numero;
+	var acumuladornegativo = 0;
+	var acumuladorpositivo = 0;
+	var contadornegativo = 0;
+	var contadorpositivo = 0;
+	var contadorceros = 0;
+	var cantidadnumeropar = 0;
+	var promediopositivo = 0;
+	var promedionegativo = 0;
+	var difpositivonegativo;
 
 while(respuesta == true)
 {
+	numero = prompt("Ingrese un número");
+	numero = parseInt(numero);
+	respuesta = confirm("¿Seguir ingresando números?");
+	
+if(numero<0)
+{
+	acumuladornegativo = acumuladornegativo + numero;
+	contadornegativo = contadornegativo + 1;
+	promedionegativo = acumuladornegativo / contadornegativo;
+}
+else if(numero>0)
+{
+	acumuladorpositivo = acumuladorpositivo + numero;
+	contadorpositivo = contadorpositivo + 1;
+	promediopositivo = acumuladorpositivo / contadorpositivo;
+}
+else if(numero == 0)
+{
+	contadorceros = contadorceros + 1;
+}
 
 }
 
-
+document.write("<h1>Suma Negativos = " + acumuladornegativo + " </h1>");
+document.write("<h1>Suma Positivos = " + acumuladorpositivo + " </h1>");
+document.write("<h1>Cantidad Positivos = " + contadorpositivo " </h1>");
+document.write("<h1>Cantidad Negativos = " + contadornegativo + " </h1>");
+document.write("<h1>Cantidad Ceros = " + contadorceros + " </h1>");
+document.write("<h1>Cantidad Números Pares = " +  + " </h1>");
+document.write("<h1>Promedios de Positivos = " + promediopositivo + " </h1>");
+document.write("<h1>Promedios de Negativos = " + promedionegativo + " </h1>");
+document.write("<h1>Diferencia entre positivos y negativos = " + (acumuladorpositivo + acumuladornegativo) + " </h1>");
 
 }//FIN DE LA FUNCIÓN
