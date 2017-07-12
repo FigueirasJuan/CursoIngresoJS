6e34386049fc1961c361de2d19ba43815527af4f
FigueirasJuan <tec.jmfigueiras@gmail.com>
2017-07-12 11:11:23 -0300

    Hasta ejercicio 10.

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,51 +1,91 @@
 function Mostrar()
 {
+    /*Al presionar el botón pedir números hasta que el usuario quiera, mostar:
+        1-Suma de los negativos.
+        2-Suma de los positivos.
+        3-Cantidad de positivos.
+        4-Cantidad de negativos.
+        5-Cantidad de ceros.
+ 
+        6-Cantidad de números pares.
+        7-Promedio de positivos.
+        8-Promedios de negativos.
+        9-Diferencia entre positivos y negativos, (positvos-negativos).
+     */
 
+	var sumaneg = 0;
+	var sumapos = 0;
+	var cantpos = 0;
+	var cantneg = 0;
+	var cantcero = 0;
+	var cantpar = 0;
+	var promneg = 0;
+	var prompos = 0;
+	var difposneg;
 	var respuesta = true;
 	var numero;
-	var acumuladornegativo = 0;
-	var acumuladorpositivo = 0;
-	var contadornegativo = 0;
-	var contadorpositivo = 0;
-	var contadorceros = 0;
-	var cantidadnumeropar = 0;
-	var promediopositivo = 0;
-	var promedionegativo = 0;
-	var difpositivonegativo;
 
 while(respuesta == true)
 {
-	numero = prompt("Ingrese un número");
+
+	numero = prompt("Ingrese un número:");
 	numero = parseInt(numero);
-	respuesta = confirm("¿Seguir ingresando números?");
 	
-if(numero<0)
-{
-	acumuladornegativo = acumuladornegativo + numero;
-	contadornegativo = contadornegativo + 1;
-	promedionegativo = acumuladornegativo / contadornegativo;
-}
-else if(numero>0)
-{
-	acumuladorpositivo = acumuladorpositivo + numero;
-	contadorpositivo = contadorpositivo + 1;
-	promediopositivo = acumuladorpositivo / contadorpositivo;
-}
-else if(numero == 0)
-{
-	contadorceros = contadorceros + 1;
+	while(isNaN(numero))
+    {
+        numero = prompt("Vuelva a ingresar un número");
+        numero = parseInt(numero);
+    }
+
+	if(numero>0)
+	{
+		sumapos = sumapos + numero;
+		cantpos++;
+	}
+
+	else if(numero<0)
+	{
+		sumaneg = sumaneg + numero;
+		cantneg++;	
+	}
+
+	else if(numero == 0)
+	{
+		cantcero++;
+	}
+
+	if(numero % 2 == 0)
+	{
+		cantpar++;
+	}
+
+	if(difposneg < 0)
+	{
+		difposneg = difposneg * -1;
+	}
+
+	else if(cantneg != 0)
+	{
+		promneg = sumaneg / cantneg;
+	}
+
+	else if(cantpos != 0)
+	{
+		prompos = sumapos / cantpos;
+	}
+
+	respuesta = confirm("¿Desea seguir ingresando números?");
+	difposneg = sumapos + sumaneg;
 }
 
-}
 
-document.write("<h1>Suma Negativos = " + acumuladornegativo + " </h1>");
-document.write("<h1>Suma Positivos = " + acumuladorpositivo + " </h1>");
-document.write("<h1>Cantidad Positivos = " + contadorpositivo " </h1>");
-document.write("<h1>Cantidad Negativos = " + contadornegativo + " </h1>");
-document.write("<h1>Cantidad Ceros = " + contadorceros + " </h1>");
-document.write("<h1>Cantidad Números Pares = " +  + " </h1>");
-document.write("<h1>Promedios de Positivos = " + promediopositivo + " </h1>");
-document.write("<h1>Promedios de Negativos = " + promedionegativo + " </h1>");
-document.write("<h1>Diferencia entre positivos y negativos = " + (acumuladorpositivo + acumuladornegativo) + " </h1>");
-
+document.write("<h1>Suma de los negativos: </h1>" + sumaneg);
+document.write("<h1>Suma de los positivos: </h1>" + sumapos);
+document.write("<h1>Cantidad de positivos: </h1>" + cantpos);
+document.write("<h1>Cantidad de negativos: </h1>" + cantneg);
+document.write("<h1>Cantidad de ceros: </h1>" + cantcero);
+document.write("<h1>Cantidad de pares: </h1>" + cantpar);
+document.write("<h1>Promedio de positivos: </h1>" + prompos);
+document.write("<h1>Promedio de negativos: </h1>" + promneg);
+document.write("<h1>Diferencia entre positivos y negativos: </h1>" + difposneg);
 }//FIN DE LA FUNCIÓN
